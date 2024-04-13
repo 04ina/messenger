c1e7fdc17e67220b20b0bf989097bf3aa2ce5158
o4ina <100081183+04ina@users.noreply.github.com>
2024-04-13 10:03:25 +0700

    Update FunctionPrototypes.h

--- SourceFiles/FunctionPrototypes.h
+++ SourceFiles/FunctionPrototypes.h
@@ -39,36 +39,3 @@ int JoinedTheChat(char UserName[31], struct UserDataType *UserDataStruct);
 void* CheckChangeStatusEverySec(void* thread_data);
 
 char *getParamsFromUserData(short int UserDataIndex, char UserData[][31]);
-
-//void GetCorrespondence(struct UserDataType *UserDataStruct, const char *UserID[1]);
-
-
-
-
-/*
-	47 | o4ina@mail.ru | 1234     | o4ina   | 12552555     | ChMesAndCor
-
-	54 | Anton@mail.ru | 123456   | Anton   | 5353535355   | ChMesAndCor
-
-	 1 |       47 |       54 | f                 | t
-
-	 от лица o4ina написать сообщение Антону
-
-	
-
-	 id                | integer                    |           | not null | nextval('message_id_seq'::regclass)
- correspondence_id | integer                    |           |          | 
- user_id           | integer                    |           |          | 
- text              | character varying(400)     |           |          | 
- date              | timestamp with time zone[] |           |          | 
-
-
-
-	!log o4ina@mail.ru 1234
-	!conmes Anton
-
-	!log Anton@mail.ru 123456
-	!conmes o4ina
-
-	!log Grigory@gmail.ru 99944f
-*/
